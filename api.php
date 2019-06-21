a18f0ff97591911453c883e5644268566225efe4
mrnobody2002 <filipemelo032@gmail.com>
2019-06-21 07:22:06 -0400

    checking parameters working

--- api.php
+++ api.php
@@ -4,5 +4,9 @@ header('content-type: application/json');
 ini_set('max_execution_time', 0);
 require './list-module.php';
 
+if(filter_input(INPUT_GET, 'q')){
+    requestListData(filter_input(INPUT_GET, 'q'));
+}else{
+    echo json_encode(array('message'=>'missing parameters'));
 
-requestListData($_GET['q']);
+}
